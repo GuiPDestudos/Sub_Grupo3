8812b63361fa77a8aa2d4ebd6dffb02370a82a7a
LucasEmmanuelRM <lucas.emmanuel10@hotmail.com>
2024-12-21 14:43:01 -0300

    Conversor de Temperatura

---
+++ temp.h
@@ -0,0 +1,16 @@
+#ifndef temp_h
+#define temp_h
+
+// Funções de conversão
+float igual(float value);
+float celsiusFahrenheit(float c);
+float celsiusKelvin(float c);
+float fahrenheitCelsius(float f);
+float fahrenheitKelvin(float f);
+float kelvinCelsius(float k);
+float kelvinFahrenheit(float k);
+
+// Função principal para executar o conversor
+void conversorDeTemperatura();
+
+#endif
