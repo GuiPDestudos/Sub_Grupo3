3cdc9d14b6a983362baf2545f7f6df1276ada087
GuiPDestudos <guipdestudos@gmail.com>
2024-12-21 12:15:02 -0300

    Add files via upload

---
+++ digital.h
@@ -0,0 +1,26 @@
+float b_kb(float byte){
+   return byte/1024;
+};
+
+float b_mb(float byte){
+    return byte/1048576;
+};
+
+float kb_B(float kilo_byte){
+    return kilo_byte*1024;
+};
+
+float kb_mb(float kilo_byte){
+    return kilo_byte/1024;
+};
+
+float mb_b(float mega_byte){
+    return mega_byte*1048576;
+};
+
+float mb_kb(float mega_byte){
+    return mega_byte*1024;
+};
+
+
+
