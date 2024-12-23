9ac006010147ff49db316dc4bd4f305e2d6a6334
GuiPD1610 <156451203+GuiPD1610@users.noreply.github.com>
2024-12-23 15:03:12 -0300

    atualizaçaodia23

--- main.c
+++ main.c
@@ -3,14 +3,10 @@
 #include "temp.h"
 
 int main(){
-
-
-
-
-
-
-
-
-    
+    double valor;
+    printf("\n\tCOVERSOR DE MEDIDAS SUB_GRUPO3\n\n");
+    printf("Escolha o valor de kilobytes a serem convertidos em kilobytes: ");
+    scanf("%.2f",valor);
+    printf("\n%.2fb = %2.fkb\n\n",valor,b_kb(valor));
     return 0;
 }
